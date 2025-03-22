a44f9d765cc8770c60fcb488a88df50b4fb74696
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2025-03-22 08:16:40 +0000

    feat(deps): update charts and images to latest (#46)

--- variables.tf
+++ variables.tf
@@ -77,7 +77,7 @@ variable "eso_image" {
 variable "eso_image_version" {
   type        = string
   description = "The version or digest for the external secrets image to deploy. If changing the value, ensure it is compatible with the chart version set in eso_chart_version."
-  default     = "v0.14.4-ubi@sha256:541b8f782b8768ff02aaf60a64982cd076d19715bc0016b22f231ea16ceae421" # datasource: ghcr.io/external-secrets/external-secrets
+  default     = "v0.15.0-ubi@sha256:a141523e18fc4d43f8f5f4d109597ca843624c917b87ba542b1fc9184a196ce4" # datasource: ghcr.io/external-secrets/external-secrets
   nullable    = false
   validation {
     condition     = can(regex("(^v\\d+\\.\\d+.\\d+(\\-\\w+)?(\\@sha256\\:\\w+){0,1})$", var.eso_image_version))
@@ -96,7 +96,7 @@ variable "eso_chart_version" {
   type        = string
   description = "The version of the External Secrets Operator Helm chart. Ensure that the chart version is compatible with the image version specified in eso_image_version."
   # renovate: datasource=github-tags depName=external-secrets/external-secrets versioning="regex:^helm-chart-(?<major>\\d+)\\.(?<minor>\\d+)\\.(?<patch>\\d+)$"
-  default  = "0.14.4"
+  default  = "0.15.0"
   nullable = false
 }
 
