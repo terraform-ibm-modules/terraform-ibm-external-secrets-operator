b46c82a22300a5d76fb0fb99539cfdbaa2a89233
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-04-26 22:22:55 +0000

    chore(deps): update terraform-module

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -208,7 +208,7 @@ module "network_acl" {
 # OCP CLUSTER creation
 module "ocp_base" {
   source               = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version              = "3.46.7"
+  version              = "3.46.8"
   cluster_name         = "${var.prefix}-vpc"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
@@ -277,7 +277,7 @@ resource "ibm_resource_instance" "secrets_manager" {
 # Additional Secrets-Manager Secret-Group for SERVICE level secrets
 module "secrets_manager_group_acct" {
   source               = "terraform-ibm-modules/secrets-manager-secret-group/ibm"
-  version              = "1.3.2"
+  version              = "1.3.3"
   region               = local.sm_region
   secrets_manager_guid = local.sm_guid
   #tfsec:ignore:general-secrets-no-plaintext-exposure
