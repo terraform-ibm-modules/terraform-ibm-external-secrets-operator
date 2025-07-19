f8a4223477c4943280085f10d68b781d8c9ecda6
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-07-19 17:29:38 +0000

    fix(deps): update terraform dependencies

--- tests/existing-resources/main.tf
+++ tests/existing-resources/main.tf
@@ -184,7 +184,7 @@ module "network_acl" {
 # OCP CLUSTER creation
 module "ocp_base" {
   source               = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version              = "3.52.0"
+  version              = "3.52.2"
   cluster_name         = "${var.prefix}-vpc"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
