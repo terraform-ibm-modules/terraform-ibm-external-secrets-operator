f5b927a8f8bf5ca833c4673bf8a4e33101fa8298
mukul-palit <mukul.palit@ibm.com>
2025-07-29 14:15:51 +0530

    test other region

--- tests/pr_test.go
+++ tests/pr_test.go
@@ -575,7 +575,7 @@ func setupOptionsSchematics(t *testing.T, prefix string, dir string) *testhelper
 
 	// # Temp workaround for : https://watson.service-now.com/nav_to.do?uri=sn_customerservice_case.do?sys_id=a9dbcdef47bae2504fc04c4a516d4372%26sysparm_view=case
 	// if region == "eu-gb" {
-	region := "us-south"
+	region := "us-east"
 	// }
 
 	logger.Log(t, "Using region: ", region)
