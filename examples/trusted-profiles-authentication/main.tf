7ad49595cc31e040872a280bdfa5e6b5fc93ad83
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-10-09 23:22:54 +0000

    chore(deps): update dependencies

--- examples/trusted-profiles-authentication/main.tf
+++ examples/trusted-profiles-authentication/main.tf
@@ -166,7 +166,7 @@ module "external_secrets" {
 
 module "vpes" {
   source   = "terraform-ibm-modules/vpe-gateway/ibm"
-  version  = "4.7.8"
+  version  = "4.7.9"
   count    = var.service_endpoints == "private" ? 1 : 0
   region   = var.region
   prefix   = "vpe"
