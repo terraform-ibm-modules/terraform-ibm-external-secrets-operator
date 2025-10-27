dae27b969389c63f13197adf99af96a3949f1726
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2025-10-27 00:24:06 +0000

    fix(deps): update dependencies

--- examples/all-combined/main.tf
+++ examples/all-combined/main.tf
@@ -186,7 +186,7 @@ module "network_acl" {
 # OCP CLUSTER creation
 module "ocp_base" {
   source               = "terraform-ibm-modules/base-ocp-vpc/ibm"
-  version              = "3.66.0"
+  version              = "3.67.3"
   cluster_name         = "${var.prefix}-vpc"
   resource_group_id    = module.resource_group.resource_group_id
   region               = var.region
@@ -231,7 +231,7 @@ data "ibm_cis" "cis_instance" {
 
 module "vpes" {
   source   = "terraform-ibm-modules/vpe-gateway/ibm"
-  version  = "4.7.12"
+  version  = "4.8.1"
   count    = var.service_endpoints == "private" ? 1 : 0
   region   = var.region
   prefix   = "vpe"
