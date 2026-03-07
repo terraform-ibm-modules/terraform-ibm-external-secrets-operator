a588d12e5d2698bc02cdf85b6f112d6d6ca9c2d6
Terraform IBM Modules Operations <Terraform.IBM.Modules.Operations@ibm.com>
2026-03-07 13:24:30 +0000

    fix(deps): update dependencies

--- examples/all-combined/main.tf
+++ examples/all-combined/main.tf
@@ -231,7 +231,7 @@ data "ibm_cis" "cis_instance" {
 
 module "vpes" {
   source   = "terraform-ibm-modules/vpe-gateway/ibm"
-  version  = "5.0.6"
+  version  = "5.0.7"
   count    = var.service_endpoints == "private" ? 1 : 0
   region   = var.region
   prefix   = "vpe"
