216ea5beeb28ca91a824d0ef6c8ee52331f4d1de
Vipin Kumar <77929205+vkuma17@users.noreply.github.com>
2026-05-23 20:27:30 +0530

    Update main.tf

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -261,7 +261,7 @@ resource "ibm_resource_instance" "secrets_manager" {
   resource_group_id = module.resource_group.resource_group_id
   service_endpoints = "public-and-private"
   parameters = {
-    "allowed_network" = "public"
+    "allowed_network" = "public-and-private"
   }
   timeouts {
     create = "30m" # Extending provisioning time to 30 minutes
