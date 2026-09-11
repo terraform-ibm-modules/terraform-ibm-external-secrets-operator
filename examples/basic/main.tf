9c59d675840d2762149991bd326d5d5710332675
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-09-12 00:39:41 +0530

    fix(deps): update dependencies (#310)

--- examples/basic/main.tf
+++ examples/basic/main.tf
@@ -91,7 +91,7 @@ module "zone_subnet_addrs" {
 
 module "vpc" {
   source                      = "terraform-ibm-modules/vpc/ibm"
-  version                     = "1.7.0"
+  version                     = "1.8.0"
   vpc_name                    = "${var.prefix}-vpc"
   resource_group_id           = module.resource_group.resource_group_id
   locations                   = []
@@ -108,7 +108,7 @@ module "vpc" {
 
 module "subnet_prefix" {
   source   = "terraform-ibm-modules/vpc/ibm//modules/vpc-address-prefix"
-  version  = "1.7.0"
+  version  = "1.8.0"
   count    = length(local.subnet_prefix)
   name     = "${var.prefix}-z-${local.subnet_prefix[count.index].label}-${split("-", local.subnet_prefix[count.index].zone)[2]}"
   location = local.subnet_prefix[count.index].zone
@@ -120,7 +120,7 @@ module "subnet_prefix" {
 module "subnets" {
   depends_on                 = [module.subnet_prefix]
   source                     = "terraform-ibm-modules/vpc/ibm//modules/subnet"
-  version                    = "1.7.0"
+  version                    = "1.8.0"
   count                      = length(local.subnet_prefix)
   location                   = local.subnet_prefix[count.index].zone
   vpc_id                     = module.vpc.vpc.vpc_id
@@ -132,7 +132,7 @@ module "subnets" {
 
 module "public_gateways" {
   source            = "terraform-ibm-modules/vpc/ibm//modules/public-gateway"
-  version           = "1.7.0"
+  version           = "1.8.0"
   count             = length(var.zones)
   vpc_id            = module.vpc.vpc.vpc_id
   location          = "${var.region}-${var.zones[count.index]}"
@@ -142,7 +142,7 @@ module "public_gateways" {
 
 module "security_group" {
   source                = "terraform-ibm-modules/vpc/ibm//modules/security-group"
-  version               = "1.7.0"
+  version               = "1.8.0"
   depends_on            = [module.vpc]
   create_security_group = false
   resource_group_id     = module.resource_group.resource_group_id
@@ -188,7 +188,7 @@ locals {
 
 module "network_acl" {
   source            = "terraform-ibm-modules/vpc/ibm//modules/network-acl"
-  version           = "1.7.0"
+  version           = "1.8.0"
   name              = "${var.prefix}-vpc-acl"
   vpc_id            = module.vpc.vpc.vpc_id
   resource_group_id = module.resource_group.resource_group_id
