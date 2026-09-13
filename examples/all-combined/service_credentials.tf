c929b5dfd2cf0d87c48f270067f3a105138c72f0
Terraform IBM Modules Operations <106112202+terraform-ibm-modules-ops@users.noreply.github.com>
2026-09-14 00:41:44 +0530

    chore(deps): update dependencies (#313)

--- examples/all-combined/service_credentials.tf
+++ examples/all-combined/service_credentials.tf
@@ -5,7 +5,7 @@
 # Create MySQL database instance using the IBM ICD MySQL module
 module "mysql_db" {
   source              = "terraform-ibm-modules/icd-mysql/ibm"
-  version             = "3.0.2"
+  version             = "3.0.3"
   name                = "${var.prefix}-mysql-db"
   region              = var.region
   deletion_protection = false
